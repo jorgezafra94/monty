6ff8a04e1690537455e762590596c676345df992
jorgezafra94 <947@holbertonschool.com>
2019-09-24 16:25:38 +0000

    [Fix] Betty style modified monty.h

--- monty.h
+++ monty.h
@@ -14,9 +14,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -29,7 +29,7 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 #endif
